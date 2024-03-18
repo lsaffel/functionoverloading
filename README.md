84e6ebe4643a2589604035df5ddbdea948d781a9
Lisa <llvvss@icloud.com>
2024-03-18 09:39:26 -0600

    3 function definitions for sum

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# functionoverloading
+Function overloading  
+3 function definitions for sum, with either int or double  
+ with 2 parameters each  
+or one with float and 3 parameters
--- src/functionoverloading.cpp
+++ src/functionoverloading.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
 
+int sum(int a, int b);
+double sum(double a, double b);
+float sum(float a, float b, float c);
+
 int main() {
+
+    cout << sum(4, 3) << endl;
 
 
     return 0;
 }
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+double sum(double a, double b) {
+    return a + b;
+}
+
+float sum(float a, float b, float c) {
+    return a + b + c;
+}
+
